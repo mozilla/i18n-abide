0d882ad7bf61438ee6e5978da04d571daf7ae00e
Austin King <shout@ozten.com>
2012-12-14 13:30:44 -0800

    Adding support for Express 3

---
+++ examples/express2/README.md
@@ -0,0 +1,1 @@
+This example demonstrates how to use i18n-abide with Express 2.
---
+++ examples/express3/app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    i18n = require('i18n-abide'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(i18n.abide({
+    supported_languages: ['en-US', 'de', 'es', 'db-LB', 'it-CH'],
+    default_lang: 'en-US',
+    debug_lang: 'it-CH',
+    locale_directory: 'locale'
+  }));
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
