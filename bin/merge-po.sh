68c02ad499d76f037e441d70e7438d2f1caa2be9
Austin King <shout@ozten.com>
2013-04-12 14:31:13 -0700

    Removing podebug, obsolete with gobbledygook

--- bin/merge-po.sh
+++ bin/merge-po.sh
@@ -18,17 +18,3 @@ for lang in `find $1 -type f -name "*.po"`; do
     msgmerge -o ${dir}/${stem}.po.tmp ${dir}/${stem}.po $1/templates/LC_MESSAGES/${stem}.pot
     mv ${dir}/${stem}.po.tmp ${dir}/${stem}.po
 done
-
-# Optionally auto-localize our test locale db-LB
-if hash podebug >/dev/null; then
-    # If your using client side gettext, add client to the list of cataglos on the next line
-    # for catalog in messages client; do
-    for catalog in messages; do
-
-        echo "Translating ${catalog}.po"
-        podebug --rewrite=flipped -i $1/templates/LC_MESSAGES/${catalog}.pot\
-               -o $1/db_LB/LC_MESSAGES/${catalog}.po
-    done
-else
-  echo 'Skipping db-LB, install translate-toolkit if you want to have that up-to-date.'
-fi
