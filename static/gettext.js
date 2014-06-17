d24e5c009438bafff88903c40f1926bc3b02c1db
Doug Beck <doug@douglasbeck.com>
2014-06-17 17:03:20 -0400

    Fix linting errors with static/gettext.js

--- static/gettext.js
+++ static/gettext.js
@@ -7,36 +7,33 @@
   "use strict";
 
   function Gettext() {
-      return {
+    return {
         gettext: function (msgid) {
           if (json_locale_data && json_locale_data.messages) {
-          var dict = json_locale_data.messages;
+            var dict = json_locale_data.messages;
             if (dict[msgid] && dict[msgid].length >= 2 &&
-                dict[msgid][1].trim() != "") {
+                dict[msgid][1].trim() !== "") {
               return dict[msgid][1];
             }
-        }
-        return msgid;
+          }
+          return msgid;
         },
         // See lib/i18n.js format docs
         format: function (fmt, obj, named) {
-          if (! fmt) return "";
+          if (! fmt) {
+            return "";
+          }
           if (! fmt.replace) {
             return fmt;
           }
           if (named) {
-            return fmt.replace(/%\(\w+\)s/g, function(match){return String(obj[match.slice(2,-2)])});
+            return fmt.replace(/%\(\w+\)s/g, function(match){return String(obj[match.slice(2,-2)]);});
           } else {
-            return fmt.replace(/%s/g, function(match){return String(obj.shift())});
+            return fmt.replace(/%s/g, function(){return String(obj.shift());});
           }
         }
       };
-  };
-
-  var params = {
-        "domain" : "client",
-        "locale_data" : json_locale_data
-  };
+  }
   var gt = new Gettext();
   window.gettext = gt.gettext.bind(gt);
   window.format = gt.format.bind(gt);
